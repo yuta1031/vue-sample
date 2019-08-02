5ecb2a364c827927c367f33ba031c19cfef5d4af
yuta <nagaiyuta@team-lab.com>
2019-08-02 15:44:14 +0900

    refactor: パス修正

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
-app.use(express.static("/dist"));
+app.use(express.static("./dist"));
 app.get("*", function(req, res) {
-  res.sendfile("/dist/index.html");
+  res.sendfile("./dist/index.html");
 });
 app.listen(port);
